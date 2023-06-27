d1516acc73858951b6cb65acc5bfca796d074b1c
Michal@MacBook <michal.marusan@gmail.com>
2023-06-27 11:21:37 +0200

    innogy init

--- frontend/src/pages/chat/Chat.tsx
+++ frontend/src/pages/chat/Chat.tsx
@@ -164,7 +164,7 @@ const Chat = () => {
                                     aria-hidden="true"
                                 />
                                 <h1 className={styles.chatEmptyStateTitle}></h1>
-                                <h2 className={styles.chatEmptyStateSubtitle}>Toto demo je nad vybranou znalostní bázi z Oriflame.cz</h2>
+                                <h2 className={styles.chatEmptyStateSubtitle}>Toto demo je nad vybranou znalostní bází</h2>
                             </Stack>
                         ) : (
                             <div className={styles.chatMessageStream} style={{ marginBottom: isLoading ? "40px" : "0px"}} role="log">
